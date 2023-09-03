a07326e572d460c32a28adbf1d45a7653ec77191
usman-2000 <usmanrahim2000@gmail.com>
2023-09-03 16:35:14 +0500

    Nfts Apis

--- server/Controller/nftController.js
+++ server/Controller/nftController.js
@@ -1,1 +1,72 @@
 const nfts = require("../Model/nftSchema");
+
+exports.nftPost = async (req, res) => {
+  const { title, price, description, ipfsHash, ownerAddress, sellerAddress } =
+    req.body;
+  if (
+    title ||
+    price ||
+    description ||
+    ipfsHash ||
+    ownerAddress ||
+    sellerAddress
+  ) {
+    res.status(400).json({ error: "All fields are required" });
+  }
+
+  try {
+    // const preUser = await users.findOne({ walletAddress: walletAddress });
+    const nftData = new nfts({
+      title,
+      price,
+      description,
+      ipfsHash,
+      ownerAddress,
+      sellerAddress,
+    });
+    await nftData.save();
+    res.status(200).json(nftData);
+  } catch (err) {
+    res.status(400).json({ error: err });
+    console.log("catch block error" + err);
+  }
+};
+
+exports.getSingleNft = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const singleNft = await users.findOne({
+      _id: id,
+    });
+    res.status(200).json(singleNft);
+  } catch (error) {
+    res.status(400).json({ error: error });
+    console.log("catch block error" + error);
+  }
+};
+
+exports.getOwnersNft = async (req, res) => {
+  const { ownerAddress } = req.params;
+  try {
+    const ownersNfts = await users.findOne({
+      ownerAddress: ownerAddress,
+    });
+    res.status(200).json(ownersNfts);
+  } catch (error) {
+    res.status(400).json({ error: error });
+    console.log("catch block error" + error);
+  }
+};
+
+exports.getAllNfts = async (req, res) => {
+  const { sellerAddress } = req.params;
+  try {
+    const ownersNfts = await users.findOne({
+      ownerAddress: ownerAddress,
+    });
+    res.status(200).json(ownersNfts);
+  } catch (error) {
+    res.status(400).json({ error: error });
+    console.log("catch block error" + error);
+  }
+};
