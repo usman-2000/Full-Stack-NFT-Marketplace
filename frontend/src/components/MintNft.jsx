195ccddbe2be0d20c48753d41e820f069f0cef37
usman-2000 <usmanrahim2000@gmail.com>
2023-09-05 13:32:23 +0500

    ipfs

--- frontend/src/components/MintNft.jsx
+++ frontend/src/components/MintNft.jsx
@@ -3,64 +3,64 @@ import React from "react";
 import "../styles/mintnft.css";
 
 const MintNft = () => {
-  // const [username, setUserName] = useState();
-  // const [email, setEmail] = useState();
-  // const [walletAddress, setWalletAddress] = useState();
+  const ethers = require("ethers");
+
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState();
+  const [description, setDescription] = useState();
+  const [ipfsHash, setIpfsHash] = useState();
+  const [ownerAddress, setOwnerAddress] = useState(); // wallet address
+  const [fileURL, setFileURL] = useState(null);
+  const [message, updateMessage] = useState("");
   const [tokenId, setTokenId] = useState(1);
+  const active = true;
+  const sellerAddress = "0x21312312312312312312312";
+  const contractAddress = "0x21312312312312312312312";
 
-  // const navigate = useNavigate();
-
-  // const handleSubmit = async (e) => {
-  //   console.log(walletAddress);
-  //   e.preventDefault();
-  //   try {
-  //     await axios
-  //       .post("http://localhost:5004/users/createuser", {
-  //         username,
-  //         email,
-  //         walletAddress,
-  //       })
-  //       .then((result) => console.log(result));
-  //     navigate("/");
-  //   } catch (error) {
-  //     console.log(error);
-  //     alert(error.response.data.error);
-  //   }
-
-  //   console.log(username, email, walletAddress);
-  //   // username && email ? navigate("/") : alert("All fields are required");
-  // };
-
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // useEffect(() => {
-  //   setWalletAddress(localStorage.getItem("address"));
-  //   console.log(walletAddress);
-  // });
   return (
     <div className="mint-comp">
       <h1>Mint Your Own Token</h1>
       <div className="mint-form">
         <h3>Your Token ID will be = {tokenId}</h3>
-        <h2></h2>
+        <h2>The Contract address is :</h2>
+        <h2> 0x3143241212300000000000021312312</h2>
+        <h2>Seller Address is : </h2>
+        <h2>{sellerAddress}</h2>
         <form>
+          <label htmlFor="">Title</label>
+          <input
+            type="text"
+            placeholder="Enter your nft title"
+            onChange={(e) => setTitle(e.target.value)}
+            required={true}
+          />
+          <label htmlFor="">Price (in ETH)</label>
+          <input
+            type="number"
+            placeholder="Min 0.01 ETH"
+            onChange={(e) => setPrice(e.target.value)}
+            required={true}
+          />
+          <label htmlFor="">Description</label>
+          <input
+            type="text"
+            placeholder="Enter the description of your nft"
+            onChange={(e) => setDescription(e.target.value)}
+            required={true}
+          />
           <label htmlFor="">Wallet Address</label>
           <input
             type="text"
             placeholder="Enter your wallet address"
-            // onChange={(e) => setUserName(e.target.value)}
+            onChange={(e) => setOwnerAddress(e.target.value)}
             required={true}
           />
 
-          <label htmlFor="">Token URI</label>
-          <input
-            type="email"
-            placeholder="Enter the token URI"
-            // onChange={(e) => setEmail(e.target.value)}
-            required={true}
-          />
+          <label>Upload Image (&lt;500 KB)</label>
+          <input type={"file"} onChange={"OnChangeFile"}></input>
           {/* <Link to="/"> */}
           <button type="submit" className="btn">
-            Submit
+            Mint and List
           </button>
           {/* </Link> */}
         </form>
